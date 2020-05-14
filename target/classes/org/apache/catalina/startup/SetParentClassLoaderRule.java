4c84b9c2d0040cf5ffbe0ba50e01a26256cc49ab
muzan <muzan@ibantang.com>
2020-05-14 11:58:36 +0800

    create

---
+++ target/classes/org/apache/catalina/startup/SetParentClassLoaderRule.java
@@ -0,0 +1,36 @@
+package org.apache.catalina.startup;
+
+import org.apache.catalina.Container;
+import org.apache.tomcat.util.digester.Rule;
+import org.xml.sax.Attributes;
+
+/**
+ * Rule that sets the parent class loader for the top object on the stack,
+ * which must be a <code>Container</code>.
+ */
+
+final class SetParentClassLoaderRule extends Rule {
+
+    public SetParentClassLoaderRule(ClassLoader parentClassLoader) {
+
+        this.parentClassLoader = parentClassLoader;
+
+    }
+
+    ClassLoader parentClassLoader = null;
+
+    @Override
+    public void begin(String namespace, String name, Attributes attributes)
+            throws Exception {
+
+        if (digester.getLogger().isDebugEnabled()) {
+            digester.getLogger().debug("Setting parent class loader");
+        }
+
+        Container top = (Container) digester.peek();
+        top.setParentClassLoader(parentClassLoader);
+
+    }
+
+
+}
